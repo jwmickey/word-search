8e7b3365a902a18580b12bf1e6511adef728e4bc
Jody Mickey <jody.mickey@gmail.com>
2023-01-01 17:24:16 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'word-search-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'word-search';
+}
